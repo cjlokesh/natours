20a9e4bfe1148a023286444e0b996a135214a1f2
cjlokesh <lokeshsanjay11@gmail.com>
2022-07-11 12:47:36 +0530

    Added dev-data with tours-simple.json

--- app.js
+++ app.js
@@ -2,16 +2,31 @@ const express = require("express");
 
 const app = express();
 
+const fs = require("fs");
 const port = 3001;
 
-app.get("/", (req, res, next) => {
-  res
-    .status(200)
-    .json({ message: "Hello from the server side!", app: "Natours" });
-});
+// app.get("/", (req, res, next) => {
+//   res
+//     .status(200)
+//     .json({ message: "Hello from the server side!", app: "Natours" });
+// });
 
-app.post("/", (req, res) => {
-  res.send("You can post to this endpoint...");
+// app.post("/", (req, res) => {
+//   res.send("You can post to this endpoint...");
+// });
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get("/api/v1/tours", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
 });
 
 app.listen(port, () => {
