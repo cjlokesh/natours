a42f5b253f4b72148dff3e52a5d6488331251d46
cjlokesh <lokeshsanjay11@gmail.com>
2022-07-21 10:07:09 +0530

    Created test schema and model

--- server.js
+++ server.js
@@ -24,13 +24,42 @@ mongoose
     console.log(`Connection Successful!`);
   });
 
+// Describe Schema / Data
+const toursSchema = new mongoose.Schema({
+  // Schema type
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: { type: Number, default: 4.5 },
+  price: { type: Number, required: [true, 'A tour must have price'] },
+});
+
+// Describe your model from tours schema
+const Tour = mongoose.model('Tour', toursSchema);
+
+const testTour = new Tour({
+  name: 'The Park Camper',
+  price: 497,
+});
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log(`Final Document ${doc}`);
+  })
+  .catch((err) => {
+    console.log(`Document insert failed due to ${err}`);
+  });
+
 // this env is set by express
 // console.log(app.get("env"));
 
 // Process ENV by default by node
 // console.log(process.env);
 
-console.log(process.env.NODE_ENV);
+// console.log(process.env.NODE_ENV);
 
 // 2. Run application
 app.listen(process.env.PORT, () => {
